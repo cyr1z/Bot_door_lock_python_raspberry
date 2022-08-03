fbf15689896883f113cd77c8827e228f4de66064
kyrylo.zolotarov@nixsolutions.com <kyrylo.zolotarov@nixsolutions.com>
2022-08-03 19:50:41 +0300

    change gpio to api call

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-# chkconfig: - 98 02                        
+# chkconfig: - 98 02
 # description:  Home telegram_bot
 # processname: bot
+import time
 import telebot, json  # , logging
-from settings import TOKEN, ADMIN, AWAPIKEY, RP5CITY, LANGRP5, AWCITY, LANGAW,\
-    YRCITY
+from settings import TOKEN, ADMIN #, AWAPIKEY, RP5CITY, LANGRP5, AWCITY, LANGAW, YRCITY
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
-from utils import open_door, pin_clean, open_dog_door, old_message
-from weather_parsers import get_rp5_weather_summary, get_own_measure, \
-    YrNow  # , Accuweather
+from utils import open_door, open_dog_door, old_message
+# from weather_parsers import get_rp5_weather_summary
+
 from lib import sticker, words
 
 # forecast = Accuweather(AWCITY, AWAPIKEY, LANGAW)
@@ -16,111 +16,108 @@ from lib import sticker, words
 # level=logging.DEBUG)
 
 bot = telebot.TeleBot(TOKEN)
-keyboard1 = telebot.types.ReplyKeyboardMarkup(row_width=2,
-                                              resize_keyboard=True)
-keyboard1.row(' 🔑 Открывай', '🌈 Погода')
-keyboard1.add('🐕 Выпустить собаку', '👁 Кто там')
+keyboard1 = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+keyboard1.row(" 🔑 Открывай", "🌈 Погода")
+keyboard1.add("🐕 Выпустить собаку", "👁 Кто там")
 try:
-    with open("/home/pi/App/BotDoorLock/data.json", "r") as read_file:
+    with open("./data.json", "r") as read_file:
         data = json.load(read_file)
 except FileNotFoundError:
-    data = {'users': [], 'rejected_users': []}
-    # logging.error('no open data.json')
+    data = {"users": [], "rejected_users": []}
 
 
 def main():
-    @bot.message_handler(commands=['start'])
+    @bot.message_handler(commands=["start"])
     def start_message(message):
-        # logging.debug(f'start from
-        # {message.chat.first_name} {message.chat.last_name}')
         yes_no_kb = InlineKeyboardMarkup(row_width=2)
         yn_buttons = [
-            InlineKeyboardButton(text='✅ Да', callback_data=message.chat.id),
-            InlineKeyboardButton(text='❌ Нет', callback_data=11)]
+            InlineKeyboardButton(text="✅ Да", callback_data=message.chat.id),
+            InlineKeyboardButton(text="❌ Нет", callback_data=11),
+        ]
         yes_no_kb.add(*yn_buttons)
 
-        if str(message.chat.id) in data['users']:
-            bot.send_sticker(message.chat.id, sticker['start'],
-                             reply_markup=keyboard1)
+        if str(message.chat.id) in data["users"]:
+            bot.send_sticker(message.chat.id, sticker["start"], reply_markup=keyboard1)
 
         else:
-            if str(message.chat.id) not in data['rejected_users']:
-                bot.send_sticker(message.chat.id, sticker['what'])
-                bot.send_message(ADMIN,
-                                 f'Добавить пользователя '
-                                 f'{message.chat.first_name} '
-                                 f'{message.chat.last_name}?',
-                                 reply_markup=yes_no_kb)
+            if str(message.chat.id) not in data["rejected_users"]:
+                bot.send_sticker(message.chat.id, sticker["what"])
+                bot.send_message(
+                    ADMIN,
+                    f"Добавить пользователя "
+                    f"{message.chat.first_name} "
+                    f"{message.chat.last_name}?",
+                    reply_markup=yes_no_kb,
+                )
 
-    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(content_types=["text"])
     def send_text(message):
-        if str(message.chat.id) in data['users'] and not old_message(
-                message.date):
-            if 'открывай' in message.text.lower():
+        if str(message.chat.id) in data["users"] and not old_message(message.date):
+            if "открывай" in message.text.lower():
                 open_door()
-                bot.send_sticker(message.chat.id, sticker['hi'])
-                pin_clean()
+                bot.send_sticker(message.chat.id, sticker["hi"])
 
-            elif 'выпустить' in message.text.lower():
+            elif "выпустить" in message.text.lower():
                 open_dog_door()
-                bot.send_sticker(message.chat.id, sticker['dog'])
-                pin_clean()
+                bot.send_sticker(message.chat.id, sticker["dog"])
 
-            elif 'кто' in message.text.lower():
-                bot.send_message(message.chat.id, 'пока не вижу')
+            elif "кто" in message.text.lower():
+                time_int = int(time.time())
+                bot.send_photo(
+                    message.chat.id,
+                    f"https://cctv-frame.zolotarev.pp.ua/screen?img={time_int}.jpg",
+                )
+            #                bot.send_video(message.chat.id, 'https://cctv-frame.zolotarev.pp.ua/gif')
 
-            elif 'погода' in message.text.lower():
-                yr_now = YrNow(YRCITY)
-                # forecast.refresh()
-                weather_message: str = get_rp5_weather_summary(RP5CITY,
-                                                               LANGRP5)
-                weather_message += get_own_measure()
-                weather_message += "\n"
-                weather_message += str(yr_now)
-                # weather_message += str(forecast)
-                bot.send_message(message.chat.id, weather_message)
-
+            elif "погода" in message.text.lower():
+                # weather_message: str = get_rp5_weather_summary(RP5CITY, LANGRP5)
+                # bot.send_message(message.chat.id, weather_message)
+                time_int = int(time.time())
+                bot.send_photo(
+                    message.chat.id,
+                    f"https://wttr.in/Kharkiv_pqQFn_lang=ru.png?t={time_int}",
+                )
             else:
                 print(message.text)
 
-    @bot.message_handler(content_types=['sticker'])
+    @bot.message_handler(content_types=["sticker"])
     def sticker_id(message):
         print(message)
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_inline(call):
-        if call.data != '11':
-            data['users'].append(call.data)
+        if call.data != "11":
+            data["users"].append(call.data)
             try:
                 with open("data.json", "w") as write_file:
                     json.dump(data, write_file)
-                    bot.send_message(ADMIN, f'Добавил {call.data}')
-                    bot.send_sticker(call.data, sticker['start'],
-                                     reply_markup=keyboard1)
+                    bot.send_message(ADMIN, f"Добавил {call.data}")
+                    bot.send_sticker(
+                        call.data, sticker["start"], reply_markup=keyboard1
+                    )
             except Exception as e:
                 # logging.exception("Exception occurred")
                 bot.send_message(ADMIN, str(e))
 
         else:
-            data['rejected_users'].append(call.data)
+            data["rejected_users"].append(call.data)
             try:
                 with open("data.json", "w") as write_file:
                     json.dump(data, write_file)
-                    bot.send_message(ADMIN, 'Отбой')
+                    bot.send_message(ADMIN, "Отбой")
 
             except Exception as e:
                 # logging.exception("Exception occurred")
                 bot.send_message(ADMIN, str(e))
 
-    pin_clean()
     bot.polling()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
-        pin_clean()
+
     except Exception as e:
         # logging.exception("Exception occurred")
         # logging.error(e)
-        pin_clean()
+        pass
--- utils.py
+++ utils.py
@@ -4,17 +4,20 @@ from time import time
 
 
 def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
+    cleanr = re.compile("<.*?>")
+    cleantext = re.sub(cleanr, "", raw_html)
     return cleantext
 
+
 def open_door():
-    response = requests.get('http://192.168.88.167:8081/door').json()
+    response = requests.get("http://192.168.88.167:8081/door").json()
     return response
 
+
 def open_dog_door():
-    response = requests.get('http://192.168.88.167:8081/dog_door').json()
+    response = requests.get("http://192.168.88.167:8081/dog_door").json()
     return response
+
 
 def old_message(message_data):
     if int(time()) - int(message_data) > 120:
